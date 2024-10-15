ac6c9b0bd13e8cd4fb5033449fcc4fdd5bd5f222
Kim Erwe <kim.erwe@student.hamk.fi>
2024-10-15 21:18:35 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,43 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //ikäohjelma
+        
+        int ika = 100 ;
+        //tulostusehdot
+        if(ika >= 0 && ika < 18){
+            System.out.println("Olet alaikäinen");
+            if(ika >= 15){
+                System.out.println("Saat ajaa mopoa.");
+                if(ika >= 16){
+                    System.out.println("Saat ajaa kevaria.");
+                }
+            }           //tämän jälkeen 18-vuotias ikärakenne
+        }
+        if(ika == 18){
+            System.out.println("Olet täysi ikäinen");
+        }
+        if(ika == 100){
+            System.out.println("Onnea");
+            System.out.println("Teille");
+            System.out.println("Paljon!");
+
+        }
+        else if(ika % 10 == 0){
+            System.out.println("Onnea tasavuosista!");
+            if(ika >= 39 && ika < 51);
+        }
+        else if(ika >= 58 && ika < 65){
+            System.out.println("Voit mennä varhaiseläkkeelle siitä.");
+        }
+        else if(ika >= 65 && ika < 100){ //korjattu ikärakenne sellaiseksi, että eläkeläistoivotuksen saa vain 65 - 99 vuotiaat.
+            System.out.println("Olet eläkeläinen.");
+            if(ika == 65){
+                System.out.println("Hyvää eläkettä!");
+            }
+        }
+        else{
+            System.out.println("Olet aikuinen");
+       }    
+        
+    }
+}
